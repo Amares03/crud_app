fd9633214e1feb0eeb3373c19380b6ad20a2e644
Amares03 <amaressabit0314@gmail.com>
2022-03-24 16:13:16 +0300

    load assets like img css js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -9,6 +11,19 @@ const PORT = process.env.PORT || 8080;
 
 //log requests
 app.use(morgan('tiny'));
+
+// parse request to body parser
+app.use(bodyParser.urlencoded({extended:true}));
+
+//set view engine
+app.set("view engine","ejs");
+
+
+// load assets
+app.use('/css',express.static(path.resolve(__dirname,"asset/css")));
+app.use('/img',express.static(path.resolve(__dirname,"asset/img")));
+app.use('/js',express.static(path.resolve(__dirname,"asset/js")));
+
 
 app.get('/',(req,res)=>{
     res.send(`server Started`);
