5e33062ed3022ee5c0f505bf978a6f0b61934cf6
Amares03 <amaressabit0314@gmail.com>
2022-03-25 15:18:42 +0300

    create api

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,6 +1,25 @@
 var Userdb = require('../model/model');
 
 // create and save user
-exports.create = (req.res)=>{
-    
-}
+// exports.create = (req,res)=>{
+//     if(!req.body){
+//         res.status(400).send
+//     }
+// }
+
+// //  find all user
+// exports.find = (req,res)=>{
+
+// }
+
+
+// // update user
+// exports.update = (req,res)=>{
+
+// }
+
+
+// // delete user
+// exports.delete = (req,res)=>{
+
+// }
--- server/routes/router.js
+++ server/routes/router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const services = require('../services/render');
 const controller = require('../controller/controller');
+var Userdb = require('../model/model');
 
 /**
  * @description Root Rout
@@ -25,6 +26,32 @@ router.get('/update-user',services.updateUserRout);
 
 
 // API
-router.post('/api/users',controller.create);
+router.post('/api/users',(req,res)=>{
+    if(!req.body){
+        res.status(400).send({message:"content cannot be empity"});
+        return;
+    }
+
+    // new user
+    const user = new Userdb({
+        name:req.body.name,
+        email:req.body.email,
+        gender:req.body.gender,
+        status:req.body.status
+    });
+
+    // save the user in the database
+    user
+    .save(user)
+    .then(data =>{
+        res.send(data);
+    })
+    .catch(err =>{
+        res.status(500).send({message:err.message || "Some Error Ocured While Creating User"});
+    });
+});
+// router.get('/api/users',controller.find);
+// router.put('/api/users',controller.update);
+// router.delete('/api/users',controller.delete);
 
 module.exports = router;
