49a5f60dc8ea9299a5502979e2b69edf62df8d4a
Amares03 <amaressabit0314@gmail.com>
2022-03-25 16:29:59 +0300

    crud api finished

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -26,18 +26,50 @@ exports.create = (req,res)=>{
     });
 }
 // //  find all user
-// exports.find = (req,res)=>{
-
-// }
+exports.find = (req,res)=>{
+    Userdb.find()
+    .then(user =>{
+        res.send(user);
+    })
+    .catch(err =>{
+        res.status(500).send({message:err.message || "Error Ocurd While Finding data"});
+    });
+}
 
 
 // // update user
-// exports.update = (req,res)=>{
+exports.update = (req,res)=>{
+    if(!req.body){
+        return res.status(400).send({message:"Update Cannot be empity"});
+    }
+    const id = req.params.id;
+    Userdb.findByIdAndUpdate(id,req.body)
+    .then(data =>{
+        if(!data){
+            res.status(404).send(`User by id ${id} not Found`);
+        }else{
+            res.send(data);
+        }
+    })
+    .catch(err =>{
+        res.status(500).send(`Error While Updating User Data Information`);
+    });
 
-// }
+}
 
 
 // // delete user
-// exports.delete = (req,res)=>{
-
-// }
+exports.delete = (req,res)=>{
+    const id = req.params.id;
+        Userdb.findByIdAndDelete(id)
+        .then(data =>{
+            if(!data){
+                res.status(404).send({message:`User by id ${id} is not found`});
+            }else{
+                res.status(200).send({message:`user is Deleted Sucssuccfully`});
+            }
+        })
+        .catch(err =>{
+            res.status(500).send({message:`user not found`});
+        });
+}
--- server/routes/router.js
+++ server/routes/router.js
@@ -27,29 +27,8 @@ router.get('/update-user',services.updateUserRout);
 
 // API
 router.post('/api/users',controller.create);
-router.get('/api/users',(req,res)=>{
-    Userdb.find()
-    .then(user =>{
-        res.send(user);
-    })
-    .catch(err =>{
-        res.status(500).send({message:err.message || "Error Ocurd While Finding data"});
-    });
-});
-router.put('/api/users/:id',(req,res)=>{
-    if(!req.body){
-        return res.status(400).send({message:"Update Cannot be empity"});
-    }
-    const id = req.params.id;
-    Userdb.findById(id)
-    .then(user =>{
-        res.send(user);
-    })
-    .catch(err =>{
-        res.status(500).send({message:err.message || "User Not Find"});
-    });
-
-});
-// router.delete('/api/users',controller.delete);
+router.get('/api/users',controller.find);
+router.put('/api/users/:id',controller.update);
+router.delete('/api/users/:id',controller.delete);
 
 module.exports = router;
